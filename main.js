2ffbf9dae0c086f90179f812af01e37c04c2a897
EeveeChan <103970972+EeveeChan@users.noreply.github.com>
2024-08-15 18:56:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarCor(){
+  document.getElementById("eusouobotao").style.backgroundColor="lightYellow"
+ document.getElementById("eusouobotao").style.color="red"
+  
+ document.getElementById("imagem").src="https://naruto-official.com/common/ogp/NTOS_OG-main.png"
+  document.getElementById("teste").innerHTML="Naruto"
+}
+
+
